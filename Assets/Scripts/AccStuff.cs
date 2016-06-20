1bfce338823f1d212aaf3237a44e232ee4bff7bc
s <student@m506-206-26376.local>
2016-06-20 14:07:49 -0700

    Split functionality more, added comments

---
+++ Assets/Scripts/AccStuff.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AccStuff {
+	public class physObj {
+		private Vector3 acceleration; //Linear acceleration from last frame
+		private Vector3 velocity; //Estimated velocity from last frame
+		private Vector3 lowBound; //Set resting acceleration
+
+		public physObj() {
+			acceleration = new Vector3(0, 0, 0);
+			velocity = new Vector3(0, 0, 0);
+			lowBound = new Vector3(0, 0, 0);
+		}
+
+		public void calcVelocity(Vector3 acc, float time) {
+			velocity = velocity + (-acceleration * 10 * time); //Discrete time interval integration of acceleration
+			acceleration = acc; //Set acceleration for next frame
+		}
+
+		public Vector3 getVelocity() {
+			return velocity;
+		}
+
+		public void setLowBound(float[] rawAcc) {
+			lowBound.x = rawAcc[1];
+			lowBound.y = rawAcc[0];
+			lowBound.z = rawAcc[2];
+		}
+
+		public void reset() {
+			acceleration = new Vector3(0, 0, 0);
+			velocity = new Vector3(0, 0, 0);
+			lowBound = new Vector3(0, 0, 0);
+		}
+			
+	}
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,32 +3,28 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using System;
 using JNIAssist;
+using AccStuff;
 
 public class Move : MonoBehaviour {
 	private Rigidbody capsule;
 	private TextMesh debug;
 	private GameObject move;
-	private bool start;
-	private float timeElapsed;
-	private int frameCount;
 	private AndroidJavaObject linacc;
 	private InitJNI jinit;
-	private Vector3 oldVel;
-	private Vector3 oldAcc;
-	private Vector3 lowbound;
+	private physObj walk;
+	private bool start;
+	private float[] rawAcc;
 
 	// Use this for initialization
 	void Start () {
 		debug = GameObject.Find("Debug").GetComponent<TextMesh>();
 		move = GameObject.Find("Capsule");
 		capsule = move.GetComponent<Rigidbody>();
+		walk = new physObj();
+		capsule.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
 		debug.text = "HELLO!";
 		start = false;
-		timeElapsed = 0;
-		capsule.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
-		oldVel = new Vector3(0, 0, 0);
-		oldAcc = new Vector3(0, 0, 0);
-		lowbound = new Vector3(0, 0, 0);
+		rawAcc = new float[] {0, 0, 0};
 
 		AndroidJNI.AttachCurrentThread();
 		jinit = new InitJNI();
@@ -37,25 +33,29 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		move.transform.rotation.eulerAngles.Set(move.transform.rotation.eulerAngles.x, this.transform.eulerAngles.y, move.transform.eulerAngles.z);
-		timeElapsed += Time.deltaTime;
+		move.transform.rotation.eulerAngles.Set(move.transform.rotation.eulerAngles.x, this.transform.eulerAngles.y, move.transform.eulerAngles.z); //Lock y rotation of rigidbody to camera
+
+		//Reset
 		if(Input.GetButtonDown("Fire1")) {
 			SceneManager.LoadScene(0);
-			lowbound = oldAcc;
+			walk.setLowBound(linacc.Call<float[]>("getAcceleration"));
 		}
+
+		//Start moving
 		if(Input.GetButtonDown("Fire2")) {
 			start = !start;
 		}
 
-		debug.text = string.Format("X: {0:0.0000}\nY: {1:0.0000}\nZ: {2:0.0000}\n MOVE: {3}", oldVel.x, oldVel.y, oldVel.z, (start ? "TRUE" : "FALSE"));
-		if(start) capsule.velocity = oldVel;
+		//Current velocity
+		debug.text = string.Format("X: {0:0.0000}\nY: {1:0.0000}\nZ: {2:0.0000}\n MOVE: {3}", rawAcc[1], rawAcc[0], rawAcc[2], (start ? "TRUE" : "FALSE"));
+
+		//Set new velocity
+		if(start) capsule.velocity = walk.getVelocity();
 		else capsule.velocity = new Vector3(0, 0, 0);
 
-		float[] lacc = linacc.Call<float[]>("getAcceleration");
-		Vector3 acc = new Vector3(-lacc[1], 0, -lacc[2]);
-		acc = acc - lowbound;
-
-		oldVel = oldVel + (oldAcc * 10 * Time.deltaTime);
-		oldAcc = acc;
+		//Priors for next frame
+		rawAcc = linacc.Call<float[]>("getAcceleration");
+		Vector3 acc = new Vector3(rawAcc[1], rawAcc[0], rawAcc[2]);
+		walk.calcVelocity(acc, Time.deltaTime);
 	}
 }
