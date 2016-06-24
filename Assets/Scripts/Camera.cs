891c9649a21299f330ed60fe87fe27052082a4c2
s <student@m506-206-26376.local>
2016-06-24 16:02:59 -0700

    Now Camera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	private TextMesh debug;
+	private WebCamDevice[] devices;
+	private WebCamTexture phoneCam;
+	private GameObject camPlane;
+
+	// Use this for initialization
+	void Start () {
+		debug = GameObject.Find("Debug").GetComponent<TextMesh>();
+		devices = WebCamTexture.devices;
+		phoneCam = new WebCamTexture();
+		camPlane = GameObject.Find("phoneCamera");
+		camPlane.GetComponent<MeshRenderer>().material.mainTexture = phoneCam;
+		phoneCam.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
