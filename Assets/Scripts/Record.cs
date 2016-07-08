4c0e25edeff042eb84df7d20332eae59fdbedd9b
s <student@m506-206-26376.local>
2016-07-07 19:54:51 -0700

    Pain

--- Assets/Scripts/Record.cs
+++ Assets/Scripts/Record.cs
@@ -6,14 +6,14 @@ using JNIAssist;
 using AccStuff;
 
 public class Record : MonoBehaviour {
-	private AndroidJavaObject linacc;
+	private LinearAcceleration linacc;
 	private InitJNI jinit;
 	private TextMesh debug;
 	private MemoryStream upload;
 	private StreamWriter fsUpload;
 	private bool record;
-	private bool data_captured;
 	private string uploadURL;
+	private float timeElapsed;
 
 	public IEnumerator sendData(WWW sendTo) {
 		yield return sendTo;
@@ -23,7 +23,7 @@ public class Record : MonoBehaviour {
 	void Start () {
 		AndroidJNI.AttachCurrentThread();
 		jinit = new InitJNI();
-		linacc = new AndroidJavaObject("hci.csumb.edu.usensors.linearAcceleration", jinit.getContext());
+		linacc = new LinearAcceleration(jinit.getContext());
 
 		debug = GameObject.Find("Debug").GetComponent<TextMesh>();
 		debug.text = "NOT RECORD";
@@ -31,36 +31,46 @@ public class Record : MonoBehaviour {
 		upload = new MemoryStream();
 		fsUpload = new StreamWriter(upload);
 		record = false;
-		data_captured = false;
+		uploadURL = "http://10.12.174.48/upload.php";
 
-		uploadURL = "http://10.12.174.79/upload.php";
+		timeElapsed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!record && !(upload.Length > 0))
+		if(Input.GetButtonDown("Fire1")) {
+			record = !record;
 			debug.text = "NOT RECORD";
-		else {
-			if(!record && data_captured) {
-				byte[] accBytes = upload.ToArray();
-				WWWForm sendAcc = new WWWForm();
-				sendAcc.AddField("frameCount", Time.frameCount.ToString());
-				string datestring = DateTime.Now.ToString("MM-dd-yy H-mm-ss");
-				sendAcc.AddBinaryData("file", accBytes, "acc" + datestring + ".csv", "text/plain");
-				WWW uploadAcc = new WWW(uploadURL, sendAcc);
-				sendData(uploadAcc);
+			GetComponent<AudioSource>().pitch = record ? 2 : 1;
+			GetComponent<AudioSource>().Play();
+
+			if(record) {
 				fsUpload.Flush();
 				upload.SetLength(0);
-				data_captured = false;
-			}
-			else {
-				debug.text = "RECORD";
-				float[] acc = linacc.Call<float[]>("getAcceleration");
-				fsUpload.WriteLine(string.Format("{0:0.0000},{1:0.0000},{2:0.0000}", acc[1], acc[0], acc[2]));
-				data_captured = true;
+				timeElapsed = 0;
 			}
 		}
-		if(Input.GetButtonDown("Fire1"))
-			record = !record;
+		if(Input.GetButtonDown("Fire2")) {
+			byte[] accBytes = upload.ToArray();
+			WWWForm sendAcc = new WWWForm();
+			sendAcc.AddField("frameCount", Time.frameCount.ToString());
+			string datestring = DateTime.Now.ToString("MM-dd-yy_H-mm-ss");
+			sendAcc.AddBinaryData("file", accBytes, "acc" + datestring + ".csv", "text/plain");
+			WWW uploadAcc = new WWW(uploadURL, sendAcc);
+			sendData(uploadAcc);
+
+			fsUpload.Flush();
+			upload.SetLength(0);
+			timeElapsed = 0;
+
+			GetComponent<AudioSource>().pitch = 3;
+			GetComponent<AudioSource>().Play();
+		}
+		if(record) {
+			debug.text = "RECORD";
+			float[] acc = linacc.accelerationRaw();
+			timeElapsed += Time.deltaTime;
+			fsUpload.WriteLine(string.Format("{0:0.0000},{1:0.0000},{2:0.0000},{3:0.0000}", acc[0], acc[1], acc[2], timeElapsed));
+		}
 	}
 }
